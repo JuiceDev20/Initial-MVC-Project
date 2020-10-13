abdd85a1ca950353416d49f435b2416090685eeb
Orlando <69806200+JuiceDev20@users.noreply.github.com>
2020-10-13 15:18:31 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Initial_MVC_Project.Models;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Initial_MVC_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Code()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Solve()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Solve(string input1, string input2)
+        {
+            var startNum = Convert.ToInt32(input1);
+            var endNum = Convert.ToInt32(input2);
+            var output = new StringBuilder();
+            for(var index = startNum; index <= endNum; index++)
+            {
+                output.AppendLine(index.ToString());
+            }
+
+            ViewData["Output"] = output.ToString();
+            return View();
+        }
+           
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
